ebda3b9e2a3a4f65ab305f66d8d09f95dd468a07
Sriram G <gsri318@gmail.com>
2025-08-04 21:16:49 +0530

    Update app.py
    this is the fully functional code for the restapi

--- app.py
+++ app.py
@@ -1,28 +1,125 @@
-from flask import Flask, request, jsonify
 
-app = Flask(__name__)
 
-# Temporary storage for one submitted JSON
-stored_data = {}
+import os
+import psycopg2
+from datetime import datetime, timezone
+from dotenv import load_dotenv
+from flask import Flask, jsonify, request
 
-# POST: Receive and store JSON
-@app.route('/submit', methods=['POST'])
-def submit_data():
-    global stored_data
+CREATE_ROOMS_TABLE =(
+    "CREATE TABLE IF NOT EXISTS rooms (id SERIAL PRIMARY KEY, name TEXT);"    
+)
+
+CREATE_TEMP_TABLE =(
+    """CREATE TABLE IF NOT EXISTS temperatues (room_id INTEGER, temp_no INTEGER,
+    date TIMESTAMP, FOREIGN KEY(room_id) REFERENCES rooms(id) ON DELETE CASCADE);""" 
+)
+
+INSERT_ROOM_RETURN_ID = "INSERT INTO rooms(name) VALUES (%s) RETURNING id;"
+
+INSERT_TEMP = "INSERT INTO temperatues (room_id, temp_no) VALUES (%s, %s);"
+
+GLOBAL_NUMBER_OF_DAYS =(
+    """ SELECT COUNT(DISTINCT DATE(date)) AS days FROM temperatues;"""
+)
+
+GLOBAL_AVG = """SELECT AVG(temp_no) AS average FROM temperatues;"""
+
+SHOW_ROOM_TEMP = "SELECT * FROM temperatues;"
+
+GET_DATA_ID = "SELECT id AS id FROM rooms"
+
+GET_DATA_NAME = "SELECT name AS name FROM rooms"
+
+load_dotenv()
+
+
+app=Flask(__name__)
+DATABASE_URL = os.getenv("DATABASE_URL")
+connection = psycopg2.connect(DATABASE_URL)
+
+
+
+@app.get("/")
+def Hello_World():
+     return jsonify({
+         "room" : "/room" ,
+        "Temparature" :  "/temparature",
+        "GetTemparature" : "/gettemp",
+        "Average" : "/average",
+        "Data" : "/datas", 
+        "Room_Datas" : "/rooms-data",
+        "Temparature_Datas" : "/temp-data"
+    })
+
+@app.post("/room")
+def create_room():
     data = request.get_json()
+    name = data["name"]
+    with connection:
+        with connection.cursor() as cursor:   
+            cursor.execute(CREATE_ROOMS_TABLE)
+            cursor.execute(INSERT_ROOM_RETURN_ID,(name,))
+            room_id = cursor.fetchone()[0]
+    return {"id": room_id, "message": f"{name} room created."}, 201
+@app.post("/temperature")
+def add_temp():
+    data = request.get_json()
+    temp_no = data["temp_no"]
+    room_id = data["room_id"]
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(CREATE_TEMP_TABLE)
+            cursor.execute(INSERT_TEMP,(room_id,temp_no))
+    return {"message" : "Temperature added."}, 201
+@app.get("/gettemp")
+def get_temp():
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(SHOW_ROOM_TEMP)
+            tempval = cursor.fetchall
+    return {"data: [{tempval}]"}
 
-    if not data:
-        return jsonify({"error": "No JSON data received"}), 400
 
-    stored_data = data  # Save the submitted JSON
-    return jsonify({"message": "Data stored successfully"}), 200
+@app.get("/average")
+def get_global_avg():
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(GLOBAL_AVG)
+            average = cursor.fetchone()[0]
+            cursor.execute(GLOBAL_NUMBER_OF_DAYS)
+            days = cursor.fetchone()[0]
+    return {"average" : round(average, 2), "days" : days}
 
-# GET: Return the stored JSON
-@app.route('/view', methods=['GET'])
-def view_data():
-    if not stored_data:
-        return jsonify({"message": "No data found"}), 404
-    return jsonify(stored_data), 200
+@app.get("/datas")
+def get_data():
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(GET_DATA_ID)
+            id = cursor.fetchone()[0]
+            cursor.execute(GET_DATA_NAME)
+            name = cursor.fetchone()[0]
+    return {"id" : id, "name" : name}
 
-if __name__ == '__main__':
-    app.run(debug=True, port= 8080)
+@app.get("/rooms-data")
+def rooms_data():
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT id, name FROM rooms;")
+            rows = cursor.fetchall()
+    data = [{"id": row[0], "name": row[1]} for row in rows]
+    return jsonify(data), 200
+
+
+@app.get("/temp-data")
+def temps_data():
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT room_id, temp_no FROM temperatues;")
+            rows = cursor.fetchall()
+    data = [{"room_id": row[0], "temp_no": row[1]} for row in rows]
+    return jsonify(data), 200
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8080)
